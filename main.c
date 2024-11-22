5290fb551c846b0e7e9f63382d4c10c3ae10e797
Pedro Chaves <pedroa.chaves10@gmail.com>
2024-11-21 21:03:08 -0300

    Update main.c
    Code correction and improvement.

--- main.c
+++ main.c
@@ -10,37 +10,41 @@ int main(){
 
     printf("Bem-vindo ao HelpDesk \n\n");
     printf("Digite o tipo de ocorrencia:\n");
-    printf("911 - Falha de conexao - Internet \n912 - Problema com impressora \n913 - Usuario sem senha \n914 - Problemas na rede interna \n915 - Computador nao liga\n");
+    printf("911 - Falha de conexao - Internet\n");
+    printf("912 - Problema com impressora\n");
+    printf("913 - Usuario sem senha\n");
+    printf("914 - Problemas na rede interna\n");
+    printf("915 - Computador nao liga\n");
     scanf("%d",&tip_ocorr);
 
     switch(tip_ocorr){
         case 911:
-        printf("Voce escolheu 911 - Falha de conexao.\n");
+        printf("\nVoce escolheu 911 - Falha de conexao.\n");
             break;
         case 912:
-        printf("Voce escolheu 912 - Problema com impressora.\n");
+        printf("\nVoce escolheu 912 - Problema com impressora.\n");
             break;
         case 913:
-        printf("Voce escolheu 913 - Usuario sem senha.\n");
+        printf("\nVoce escolheu 913 - Usuario sem senha.\n");
             break;
         case 914:
-        printf("Voce escolheu 914 - Problemas na rede interna.\n");
+        printf("\nVoce escolheu 914 - Problemas na rede interna.\n");
             break;
         case 915:
-        printf("Voce escolheu 915 - Computador nao liga.\n");
+        printf("\nVoce escolheu 915 - Computador nao liga.\n");
             break;
         default:
-            printf("Opcao invalida.\n");
+            printf("\nOpcao invalida.\n");
             break;
     }
-        printf("Identifique o seu departamento:\n");
-        printf("1 - Recepcao.\n");
-        printf("2 - Equipe de T.I.\n");
-        printf("3 - Financeiro.\n");
-        printf("4 - Desenvolvedores.\n");
-        printf("5 - Recursos Humanos.\n");
-        scanf("%d",&dep);
-        printf("Nome do solicitante do chamado:");
+    printf("Identifique o seu departamento:\n");
+    printf("1 - Recepcao.\n");
+    printf("2 - Equipe de T.I.\n");
+    printf("3 - Financeiro.\n");
+    printf("4 - Desenvolvedores.\n");
+    printf("5 - Recursos Humanos.\n");
+    scanf("%d",&dep);
+    printf("\nNome do solicitante do chamado:");
         getchar();
         fgets(nome_sol, sizeof(nome_sol), stdin);
         if (nome_sol[strlen(nome_sol)] == '\n') {
