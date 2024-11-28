7b31a326dd29dc581b638ffaff5e180aafd928b1
Pedro Chaves <pedroa.chaves10@gmail.com>
2024-11-27 21:23:09 -0300

    Update main.c

--- main.c
+++ main.c
@@ -2,46 +2,29 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#include "Funções/funcoes.h"
+#include "FunÃ§Ãµes/funcoes.h"
 
 int main() {
     int opcao;
-    char tip_ocorr;
 
     do {
-        printf("\n===Bem-vindo ao HelpDesk ===\n");
-        printf("\n=== Menu Principal ===\n");
+        printf("\n=== Bem-vindo ao HelpDesk ===\n\n");
+        printf("=== Menu Principal ===\n");
         printf("1. Lancar Chamados\n");
         printf("2. Exibir total de ocorrencias do dia\n");
         printf("3. Exibir ocorrencia mais solicitada\n");
         printf("4. Listar arquivo TXT com ocorrencias\n");
         printf("5. Finalizar programa\n");
-        printf("Escolha uma opcao:");
+        printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
         printf("\n");
 
         switch (opcao) {
             case 1:
                 cadastrarChamado();
-                    switch(opc){
-             case 911:
-        printf("\nVoce escolheu 911 - Falha de conexao.\n");
-            break;
-        case 912:
-        printf("\nVoce escolheu 912 - Problema com impressora.\n");
-            break;
-        case 913:
-        printf("\nVoce escolheu 913 - Usuario sem senha.\n");
-            break;
-        case 914:
-        printf("\nVoce escolheu 914 - Problemas na rede interna.\n");
-            break;
-        case 915:
-        printf("\nVoce escolheu 915 - Computador nao liga.\n");
-            break;
-                    }
+                break;
             case 2:
-                exibirTotalChamados();
+                printf("Total de chamados do dia: %d\n", totalChamados);
                 break;
             case 3:
                 exibirOcorrenciaMaisSolicitada();
@@ -53,9 +36,9 @@ int main() {
                 printf("Encerrando programa...\n");
                 break;
             default:
-                printf("Opção inválida!\n");
+                printf("Opcao invalida!\n");
         }
-    } while(opcao != 5);
+    } while (opcao != 5);
 
-    return (0);
+    return 0;
 }
